66ba4ed5ded07167bab5a77e351d5e9d74fd5d83
Hans Baier <hansfbaier@gmail.com>
2022-08-15 12:07:26 +0700

    global.fs: add DMA constant, remove unused base save/restore

--- global.fs
+++ global.fs
@@ -2,6 +2,7 @@
 
 $20000000 constant RAMS
 $20020000 constant RAMX
+$20030000 constant DMA
 
 $40001000 constant R8_SAFE_ACCESS
 $40001001 constant R8_CHIP_ID       \ chip ID
@@ -82,64 +83,63 @@ PFIC $4C  + constant R32_PFIC_GISR      \ PFIC interrupt global status register
 PFIC $100 + constant R32_PFIC_IENR      \ PFIC interrupt enable setting register
 PFIC $180 + constant R32_PFIC_IRER      \ PFIC interrupt enable reset register
 
-base @ decimal
-1 constant  Reset_IRQn
-2 constant  NMI_IRQn
-3 constant  EXC_IRQn
-12 constant SysTick_IRQn
-14 constant SWI_IRQn
-16 constant WDOG_IRQn
-17 constant TMR0_IRQn
-18 constant GPIO_IRQn
-19 constant SPI0_IRQn
-20 constant USBSS_IRQn
-21 constant LINK_IRQn
-22 constant TMR1_IRQn
-23 constant TMR2_IRQn
-24 constant UART0_IRQn
-25 constant USBHS_IRQn
-26 constant EMMC_IRQn
-27 constant DVP_IRQn
-28 constant HSPI_IRQn
-29 constant SPI1_IRQn
-30 constant UART1_IRQn
-31 constant UART2_IRQn
-32 constant UART3_IRQn
-33 constant SERDES_IRQn
-34 constant ETH_IRQn
-35 constant PMT_IRQn
-36 constant ECDC_IRQn
+#1  constant  Reset_IRQn
+#2  constant  NMI_IRQn
+#3  constant  EXC_IRQn
+#12 constant SysTick_IRQn
+#14 constant SWI_IRQn
+#16 constant WDOG_IRQn
+#17 constant TMR0_IRQn
+#18 constant GPIO_IRQn
+#19 constant SPI0_IRQn
+#20 constant USBSS_IRQn
+#21 constant LINK_IRQn
+#22 constant TMR1_IRQn
+#23 constant TMR2_IRQn
+#24 constant UART0_IRQn
+#25 constant USBHS_IRQn
+#26 constant EMMC_IRQn
+#27 constant DVP_IRQn
+#28 constant HSPI_IRQn
+#29 constant SPI1_IRQn
+#30 constant UART1_IRQn
+#31 constant UART2_IRQn
+#32 constant UART3_IRQn
+#33 constant SERDES_IRQn
+#34 constant ETH_IRQn
+#35 constant PMT_IRQn
+#36 constant ECDC_IRQn
 
 [ifndef] lite
 
 : interrupt-name. ( irqnum -- )
   case
-     1 of ." Reset_IRQn"   endof
-     2 of ." NMI_IRQn"     endof
-     3 of ." EXC_IRQn"     endof
-    12 of ." SysTick_IRQn" endof
-    14 of ." SWI_IRQn"     endof
-    16 of ." WDOG_IRQn"    endof
-    17 of ." TMR0_IRQn"    endof
-    18 of ." GPIO_IRQn"    endof
-    19 of ." SPI0_IRQn"    endof
-    20 of ." USBSS_IRQn"   endof
-    21 of ." LINK_IRQn"    endof
-    22 of ." TMR1_IRQn"    endof
-    23 of ." TMR2_IRQn"    endof
-    24 of ." UART0_IRQn"   endof
-    25 of ." USBHS_IRQn"   endof
-    26 of ." EMMC_IRQn"    endof
-    27 of ." DVP_IRQn"     endof
-    28 of ." HSPI_IRQn"    endof
-    29 of ." SPI1_IRQn"    endof
-    30 of ." UART1_IRQn"   endof
-    31 of ." UART2_IRQn"   endof
-    32 of ." UART3_IRQn"   endof
-    33 of ." SERDES_IRQn"  endof
-    34 of ." ETH_IRQn"     endof
-    35 of ." PMT_IRQn"     endof
-    36 of ." ECDC_IRQn"    endof
+     #1 of ." Reset_IRQn"   endof
+     #2 of ." NMI_IRQn"     endof
+     #3 of ." EXC_IRQn"     endof
+    #12 of ." SysTick_IRQn" endof
+    #14 of ." SWI_IRQn"     endof
+    #16 of ." WDOG_IRQn"    endof
+    #17 of ." TMR0_IRQn"    endof
+    #18 of ." GPIO_IRQn"    endof
+    #19 of ." SPI0_IRQn"    endof
+    #20 of ." USBSS_IRQn"   endof
+    #21 of ." LINK_IRQn"    endof
+    #22 of ." TMR1_IRQn"    endof
+    #23 of ." TMR2_IRQn"    endof
+    #24 of ." UART0_IRQn"   endof
+    #25 of ." USBHS_IRQn"   endof
+    #26 of ." EMMC_IRQn"    endof
+    #27 of ." DVP_IRQn"     endof
+    #28 of ." HSPI_IRQn"    endof
+    #29 of ." SPI1_IRQn"    endof
+    #30 of ." UART1_IRQn"   endof
+    #31 of ." UART2_IRQn"   endof
+    #32 of ." UART3_IRQn"   endof
+    #33 of ." SERDES_IRQn"  endof
+    #34 of ." ETH_IRQn"     endof
+    #35 of ." PMT_IRQn"     endof
+    #36 of ." ECDC_IRQn"    endof
   endcase
 ;
 
@@ -196,8 +196,6 @@ base @ decimal
   then
   r> R32_PFIC_ITHRESDR !
 ;
-
-base !
 
 : safe-access-mode-on ( -- )
   $57 R8_SAFE_ACCESS c!
